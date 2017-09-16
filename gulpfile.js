b9e14ef7d70fd0de1068d99f48d7220031cbf538
Joe Joiner <joejoinerr@gmail.com>
2017-09-16 23:22:19 +0100

    Remove unnecessary gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,6 @@ var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins();
 var pngquant = require('imagemin-pngquant');
-var browserSync = require('browser-sync').create();
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 var brandColors = require('postcss-brand-colors');
@@ -31,10 +30,8 @@ var reload = browserSync.reload;
 
 var paths = {
   src: './src/',
-  tmp: './.tmp/',
   dist: './dist/',
   html: '**/*.html',
-  sass: 'sass/**/*.scss',
   css: 'css/**/*.css',
   js: 'js/**/*.js',
   img: 'img/**/*.+(png|jpg|svg|gif)'
@@ -49,33 +46,11 @@ var paths = {
 \*------------------------------------*/
 
 /**
- * Render HTML
- */
-
-gulp.task('njk', function() {
-  return gulp.src(paths.src + '**/*.njk')
-    .pipe(plugins.nunjucksRender({
-      path: ['src']
-    }))
-    .pipe(gulp.dest('./', { cwd: paths.tmp }))
-});
-
-/**
- * Copy HTML to tmp folder
+ * Copy HTML to build folder
  */
 
 gulp.task('html', function() {
   return gulp.src(paths.src + paths.html)
-    .pipe(gulp.dest('./', { cwd: paths.tmp }))
-});
-
-
-/**
- * Copy HTML to build folder
- */
-
-gulp.task('html:dist', ['njk', 'html'], function() {
-  return gulp.src(paths.tmp + paths.html)
     .pipe(gulp.dest('./', { cwd: paths.dist }))
 });
 
@@ -88,68 +63,25 @@ gulp.task('html:dist', ['njk', 'html'], function() {
 \*------------------------------------*/
 
 /**
- * Compile sass
+ * Compile CSS
  */
 
-gulp.task('sass', function() {
-  var sassOptions = {
-    errLogToConsole: true,
-    includePaths: ['./node_modules'],
-    outputStyle: 'expanded',
-    precision: 2
-  };
-
+gulp.task('css', function() {
   var postcssPlugins = [
-    brandColors(),
     autoprefixer(),
     cssnano()
   ]
 
-  return gulp.src(paths.src + paths.sass)
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.sass(sassOptions).on('error', plugins.sass.logError))
-    .pipe(plugins.postcss(postcssPlugins))
-    .pipe(plugins.sourcemaps.write('./maps', { cwd: paths.tmp }))
-    .pipe(gulp.dest('./css', { cwd: paths.tmp }))
-    .pipe(browserSync.stream())
-});
-
-
-/**
- * Lint Sass
- */
-
-gulp.task('lint', function() {
-  // Options vars
-  var scssLintOptions = {
-    config: 'scss-lint.yml',
-  }
-
-  return gulp.src(paths.src + paths.sass)
-    .pipe(plugins.cached('plugins.scssLint'))
-    .pipe(plugins.scssLint(scssLintOptions))
-});
-
-
-/**
- * Minify CSS
- */
-
-gulp.task('css:dist', ['sass'], function() {
   var cleanCSSOptions = {
     debug: true,
     rebase: false
   }
 
-  return gulp.src(paths.tmp + paths.css)
+  return gulp.src(paths.src + paths.css)
+    .pipe(plugins.postcss(postcssPlugins))
     .pipe(plugins.cleancss(cleanCSSOptions))
-    .pipe(plugins.rev())
     .pipe(gulp.dest('./css', { cwd: paths.dist }))
-    .pipe(plugins.rev.manifest({
-      base: paths.dist,
-      merge: true
-    }))
-    .pipe(gulp.dest('./css', { cwd: paths.dist }))
+    .pipe(browserSync.stream())
 });
 
 
@@ -161,53 +93,19 @@ gulp.task('css:dist', ['sass'], function() {
 \*------------------------------------*/
 
 /**
- * Copy images
- */
-
-gulp.task('img', function() {
-  return gulp.src(paths.src + paths.img)
-    .pipe(gulp.dest('./img', { cwd: paths.tmp }))
-});
-
-
-/**
  * Minify images
  */
 
-gulp.task('img:dist', ['img'], function() {
+gulp.task('img', function() {
   var imageminPlugins = [
     plugins.imagemin.jpegtran({ progressive: true }),
     plugins.imagemin.gifsicle({ interlaced: true }),
     pngquant()
   ]
 
-  return gulp.src(paths.tmp + paths.img)
+  return gulp.src(paths.src + paths.img)
     .pipe(plugins.imagemin(imageminPlugins, { verbose: true }))
     .pipe(gulp.dest('./img', { cwd: paths.dist }))
-});
-
-
-
-
-
-/*------------------------------------*\
-  #SERVER
-\*------------------------------------*/
-
-/**
- * Create servera and watch files
- */
-
-gulp.task('serve', ['sass', 'html', 'njk', 'img'], function() {
-  browserSync.init({
-    server: {
-      baseDir: paths.tmp
-    }
-  });
-
-  gulp.watch(paths.sass, { cwd: paths.src }, ['sass']);
-  gulp.watch(paths.html, { cwd: paths.src }, ['html']);
-  gulp.watch(paths.html, { cwd: paths.tmp }).on('change', reload);
 });
 
 
@@ -219,23 +117,7 @@ gulp.task('serve', ['sass', 'html', 'njk', 'img'], function() {
 \*------------------------------------*/
 
 /**
- * Temporary compile
+ * Compile
  */
 
-gulp.task('compile', ['sass', 'html', 'njk', 'img'])
-
-
-/**
- * Compile for production and version files
- */
-
-gulp.task('dist', ['css:dist', 'html:dist', 'img:dist'], function() {
-  var manifest = gulp.src(paths.dist + 'rev-manifest.json')
-
-  return gulp.src(paths.tmp + paths.html)
-    .pipe(plugins.revReplace({
-      manifest: manifest,
-      replaceInExtensions: ['.html']
-    }))
-    .pipe(gulp.dest(paths.dist))
-});
+gulp.task('dist', ['css', 'html', 'img'])
