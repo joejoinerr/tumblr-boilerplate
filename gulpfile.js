8e17511bdd07be4347cf5ac737071e23f4949aa2
Joe Joiner <joejoinerr@gmail.com>
2017-10-26 23:24:42 +0100

    Add second build stage to allow for testing

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ var cssnano = require('cssnano');
 
 var paths = {
   src: './src/',
+  tmp: './tmp/',
   dist: './dist/',
   html: '**/*.html',
   css: 'css/**/*.css',
@@ -47,23 +48,29 @@ var paths = {
  * Copy HTML to build folder
  */
 
-gulp.task('html', ['css'], function() {
-  var inlineSourceOptions = {
-    compress: false,
-    rootpath: paths.dist
-  };
+ var errorHandler = function (err) {
+   console.log('Error: ' + err);
+   this.emit('end')
+ }
 
+var inlineSourceOptions = {
+  compress: false,
+  rootpath: paths.dist
+};
+
+gulp.task('html', ['css:dist'], function() {
   return gulp.src(paths.src + paths.html)
     .pipe(plugins.inlineSource(inlineSourceOptions))
     .pipe(plugins.tumblrThemeParser({ data: './tumblr-data.json' }))
     .on('error', errorHandler)
-    .pipe(gulp.dest('./', { cwd: paths.dist }))
+    .pipe(gulp.dest('./', { cwd: paths.tmp }))
 });
 
-var errorHandler = function (err) {
-  console.log('Error: ' + err);
-  this.emit('end')
-}
+gulp.task('html:dist', ['css:dist'], function() {
+  return gulp.src(paths.src + paths.html)
+    .pipe(plugins.inlineSource(inlineSourceOptions))
+    .pipe(gulp.dest('./', { cwd: paths.dist }))
+});
 
 
 
@@ -77,12 +84,12 @@ var errorHandler = function (err) {
  * Compile CSS
  */
 
-gulp.task('css', function() {
-  var postcssPlugins = [
-    autoprefixer(),
-    cssnano()
-  ]
+var postcssPlugins = [
+  autoprefixer(),
+  cssnano()
+]
 
+gulp.task('css:dist', function() {
   return gulp.src(paths.src + paths.css)
     .pipe(plugins.postcss(postcssPlugins))
     .pipe(gulp.dest('./css', { cwd: paths.dist }))
@@ -109,6 +116,11 @@ gulp.task('img', function() {
 
   return gulp.src(paths.src + paths.img)
     .pipe(plugins.imagemin(imageminPlugins, { verbose: true }))
+    .pipe(gulp.dest('./img', { cwd: paths.tmp }))
+});
+
+gulp.task('img:dist', ['img'], function() {
+  return gulp.src(paths.tmp + paths.img)
     .pipe(gulp.dest('./img', { cwd: paths.dist }))
 });
 
@@ -124,4 +136,5 @@ gulp.task('img', function() {
  * Compile
  */
 
-gulp.task('dist', ['html', 'img'])
+gulp.task('build', ['html', 'img'])
+gulp.task('dist', ['html:dist', 'img'])
